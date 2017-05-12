91bc2e41cf8f730d79d9e050233037c520282320
noviny <novinyarts@gmail.com>
2017-05-12 22:25:33 +1000

    switch to using child process to move files

--- index.js
+++ index.js
@@ -1,80 +1,24 @@
 #! /usr/bin/env node
-
 const fs = require('fs');
 const path = require('path');
-
-const baseDirs = [
-    'models',
-    'routes',
-    'updates',
-    'templates',
-]
-
-const nestedDirs = [
-    'routes/views',
-    'templates/views',
-];
-
-const neededFiles = [
-    'keystone.js',
-    'package.json',
-    '.gitignore',
-    'updates/0.0.1-first-user.js',
-    'routes/index.js',
-    'routes/views/index.js',
-    'models/User.js',
-    'templates/views/index.pug'
-]
-
-function copyFile(src, dest) {
-    return new Promise((resolve, reject) => {
-        let readStream = fs.createReadStream(src);
-
-        readStream.once('error', (err) => {
-            reject(err);
-        });
-
-        readStream.once('end', () => {
-            resolve();
-        });
-
-        readStream.pipe(fs.createWriteStream(dest));
-    })
-}
-
-const makeNewDir = (newDir) => {
-    return new Promise((resolve, reject) => {
-        fs.mkdir(newDir, err => {
-            if (err) return reject(err);
-            resolve();
-        });
-    });
-};
+const spawn = require('child_process').spawn;
 
 const allThatSetup = () => {
-    const baseDir = path.join(process.env.PWD, process.argv[2]);
+    const originDir = path.join(__dirname, 'src/mvk');
+    const newDir = path.join(process.env.PWD, process.argv[2]);
+    const scriptPath = path.join(__dirname, 'scripts/mvk.bash');
 
-    fs.access(baseDir, (err) => {
-      if(err.errno !== -2) throw new Error('The directory already exists! This process would overwrite it.')
-      makeNewDir(baseDir)
-        .then(() => baseDirs.map(dir => {
-            const newDir = path.join(baseDir, dir);
-            return makeNewDir(newDir)
-        }))
-        .then((res) => Promise.all(res))
-        .then(() => nestedDirs.map(dir => {
-            const newDir = path.join(baseDir, dir);
-            return makeNewDir(newDir)
-        }))
-        .then((res) => Promise.all(res))
-        .then(() => neededFiles.map(fileName => {
-            const fileToCopy = path.join(__dirname, 'node_modules/minimum-viable-keystone', fileName)
-            const newFile = path.join(baseDir, fileName);
-            return copyFile(fileToCopy, newFile);
-        }))
-        .then((res) => Promise.all(res))
-        .then(() => console.log('finished'))
-        .catch(err => console.log('something went wrong', err))
+    fs.access(newDir, (err) => {
+        if(err.errno !== -2) throw new Error('The directory already exists! This process would overwrite it.')
+
+        const cp = spawn('cp', ['-r', originDir, newDir]);
+        cp.stderr.on('data', (data) => {
+          console.log(`stderr: ${data}`);
+        });
+
+        cp.on('close', (code) => {
+            console.log(`Completed Setting up your files. Run 'cd ${process.argv[2]} && yarn' to complete your setup.`);
+        });
     });
 }
 
