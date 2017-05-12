9c72bb1648852448b3817f4342f055ee354fd2ca
noviny <novinyarts@gmail.com>
2017-05-12 19:33:39 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+#! /usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const baseDirs = [
+    'models',
+    'routes',
+    'updates',
+]
+
+const neededFiles = [
+    'keystone.js',
+    'package.json',
+    'updates/0.0.1-first-user.js',
+    'routes/index.js',
+    'models/User.js'
+]
+
+function copyFile(src, dest) {
+    return new Promise((resolve, reject) => {
+        let readStream = fs.createReadStream(src);
+
+        readStream.once('error', (err) => {
+            reject(err);
+        });
+
+        readStream.once('end', () => {
+            resolve();
+        });
+
+        readStream.pipe(fs.createWriteStream(dest));
+    })
+}
+
+const makeNewDir = (newDir) => {
+    return new Promise((resolve, reject) => {
+        fs.mkdir(newDir, err => {
+            if (err) return reject(err);
+            resolve();
+        });
+    });
+};
+
+const allThatSetup = () => {
+    const baseDir = path.join(process.env.PWD, process.argv[2]);
+
+    fs.access(baseDir, (err) => {
+      if(err.errno !== -2) throw new Error('The directory already exists! This process would overwrite it.')
+      makeNewDir(baseDir)
+        .then(() => baseDirs.map(dir => {
+            const newDir = path.join(baseDir, dir);
+            return makeNewDir(newDir)
+        }))
+        .then((res) => Promise.all(res))
+        .then(() => neededFiles.map(fileName => {
+            const fileToCopy = path.join(__dirname, 'node_modules/minimum-viable-keystone', fileName)
+            const newFile = path.join(baseDir, fileName);
+            return copyFile(fileToCopy, newFile);
+        }))
+        .then((res) => Promise.all(res))
+        .then(() => console.log('finished'))
+        .catch(err => console.log('something went wrong', err))
+    });
+}
+
+allThatSetup();
---
+++ planning.md
@@ -0,0 +1,9 @@
+# KESCLI (Keystone 4 CLI Tool)
+
+## Major features
+
+- Generate a Keystone MVK file structure
+- Set up keystone with react, webpack
+- Add and modify KSJS settings in keystone.js file
+- Add/remove routes
+- 
