4db83babcbf8d34a1215e3edbc9a4a9b96dd0f01
noviny <novinyarts@gmail.com>
2017-05-17 00:07:06 +1000

    can now generate react project badly

--- index.js
+++ index.js
@@ -2,29 +2,59 @@
 const fs = require('fs');
 const path = require('path');
 const spawn = require('child_process').spawn;
+const availableStarters = ['mvk', 'react-spa'];
 
-const allThatSetup = () => {
-    const originDir = path.join(__dirname, 'src/mvk');
-    const newDir = path.join(process.env.PWD, process.argv[2]);
-    const scriptPath = path.join(__dirname, 'scripts/mvk.bash');
-
-    fs.access(newDir, err => {
-        if (err.errno !== -2)
-            throw new Error(
-                'The directory already exists! This process would overwrite it.'
-            );
-
-        const cp = spawn('cp', ['-r', originDir, newDir]);
-        cp.stderr.on('data', data => {
-            console.log(`stderr: ${data}`);
-        });
-
-        cp.on('close', code => {
-            console.log(
-                `Completed Setting up your files. Run 'cd ${process.argv[2]} && yarn' to complete your setup.`
-            );
+const checkPath = dir => {
+    return new Promise((resolve, reject) => {
+        fs.access(dir, err => {
+            if (!err) {
+                console.log('problem of no error');
+                return reject(
+                    new Error({
+                        reason: 'destination directory already exists.'
+                    })
+                );
+            }
+            if (err.errno !== -2) return reject(new Error(err));
+            return resolve();
         });
     });
 };
 
-allThatSetup();
+const copyFiles = (destination, origin) => {
+    return new Promise((resolve, reject) => {
+        const cp = spawn('cp', ['-r', origin, destination]);
+        cp.stderr.on('data', data => reject(new Error({ err: data })));
+        cp.on('close', () => {
+            console.log('complete');
+            resolve();
+        });
+    });
+};
+
+const allThatSetup = (newDir, oldDir = 'mvk') => {
+    if (availableStarters.indexOf(oldDir) < 0)
+        return Promise.reject({
+            reason: `Your starter choice must be from [${availableStarters}]`
+        });
+    const origin = path.join(__dirname, `src/${oldDir}`);
+    const destination = path.join(process.env.PWD, newDir);
+    const scriptPath = path.join(__dirname, 'scripts/mvk.bash');
+
+    return checkPath(destination)
+        .then(() => copyFiles(destination, origin))
+        .then(
+            console.log(
+                `Completed Setting up your files. Run 'cd ${destination} && yarn' to complete your setup.`
+            )
+        )
+        .catch(err => {
+            console.error('There was an error setting up your project:');
+            console.log(JSON.stringify(err));
+            if (err && err.reason) console.error(err.reason);
+            if (err && err.err) console.error(err.err);
+            if (err && !err.reason && !err.err) console.log(err);
+        });
+};
+
+allThatSetup(process.argv[2], process.argv[3]);
